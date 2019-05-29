20d24ae76800c9f278e1a064c45b2cb6b12a46b0
mcferrenm <mcferrenmax@gmail.com>
2019-05-29 18:22:40 -0400

    Structs completed

--- structs/lib.h
+++ structs/lib.h
@@ -3,11 +3,35 @@
 
 #include <string.h>
 
-char *strdup(const char *);
-
-char *string_dup(char *src)
+int string_length(char *s)
 {
-  return strdup(src);
+    int length = 0;
+    for (int i = 0; ; i++) {
+        length += 1;
+        if (s[i] == '\0') {
+            break;
+        }
+    }
+    return length - 1;
+}
+
+void string_copy(char *x, char *y)
+{
+    int len = string_length(y);
+    for (int i = 0; i < len; i++) {
+        x[i] = y[i];
+    }
+    x[len] = '\0';
+}
+
+char *string_dup(char *src) 
+{
+  int src_len = string_length(src);
+  char *dup = malloc((src_len + 1) * sizeof(char));
+  
+  string_copy(dup, src);
+  
+  return dup;
 }
 
 #endif

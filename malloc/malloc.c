4693de31129816330cc5ea4e7bcd97aa2e3ba021
mcferrenm <mcferrenmax@gmail.com>
2019-05-29 18:12:34 -0400

    mem_copy WIP

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -51,7 +51,12 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
+    char *s = (char *)src; 
+    char *d = (char *)dest;
+    
+    for (int i = 0; i < n; i++) {
+      d[i] = s[i];
+    }
 }
 
 /*

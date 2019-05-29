176e126fc56b1679c4f12e617d587977fc4afe80
mcferrenm <mcferrenmax@gmail.com>
2019-05-29 17:37:44 -0400

    Malloc string_dup

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -10,9 +10,36 @@
     You may want to use the string_length function to figure out the
     length of the input string.
 */
-char *string_dup(char *src)
-{
 
+int string_length(char *s)
+{
+    int length = 0;
+    for (int i = 0; ; i++) {
+        length += 1;
+        if (s[i] == '\0') {
+            break;
+        }
+    }
+    return length - 1;
+}
+
+void string_copy(char *x, char *y)
+{
+    int len = string_length(y);
+    for (int i = 0; i < len; i++) {
+        x[i] = y[i];
+    }
+    x[len] = '\0';
+}
+
+char *string_dup(char *src) 
+{
+  int src_len = string_length(src);
+  char *dup = malloc((src_len + 1) * sizeof(char));
+  
+  string_copy(dup, src);
+  
+  return dup;
 }
 
 /*

68f0e0e9701cdf77f842453757a5887e266e30d2
mcferrenm <mcferrenmax@gmail.com>
2019-05-30 16:20:11 -0400

    mem_copy passing

--- malloc/lib.h
+++ malloc/lib.h
@@ -5,7 +5,23 @@
 
 int string_length(char *s)
 {
-  return strlen(s);
+    int length = 0;
+    for (int i = 0; ; i++) {
+        length += 1;
+        if (s[i] == '\0') {
+            break;
+        }
+    }
+    return length - 1;
+}
+
+void string_copy(char *x, char *y)
+{
+    int len = string_length(y);
+    for (int i = 0; i < len; i++) {
+        x[i] = y[i];
+    }
+    x[len] = '\0';
 }
 
 #endif
--- malloc/malloc.c
+++ malloc/malloc.c
@@ -11,27 +11,6 @@
     length of the input string.
 */
 
-int string_length(char *s)
-{
-    int length = 0;
-    for (int i = 0; ; i++) {
-        length += 1;
-        if (s[i] == '\0') {
-            break;
-        }
-    }
-    return length - 1;
-}
-
-void string_copy(char *x, char *y)
-{
-    int len = string_length(y);
-    for (int i = 0; i < len; i++) {
-        x[i] = y[i];
-    }
-    x[len] = '\0';
-}
-
 char *string_dup(char *src) 
 {
   int src_len = string_length(src);
